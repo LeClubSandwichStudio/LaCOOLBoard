005ef6ebfec130540cca4f4e298432d2d7eab7fa
Simon Juif <simon@lacool.co>
2018-01-08 18:14:08 +0100

    Add a second example, automaticly pour your plant

---
+++ examples/AutoSprinkel/AutoSprinkel.ino
@@ -0,0 +1,46 @@
+/**
+*	CoolBoardExample	
+*
+*	This example shows typical use
+*	of the CoolBoard.
+*		
+*	Save this example in another location
+*	in order to safely modify the configuration files
+*	in the data folder.
+*
+*/
+
+#include <CoolBoard.h>
+
+
+CoolBoard coolBoard;
+
+
+void setup()
+{
+	Serial.begin(115200);
+	coolBoard.config();
+	
+	coolBoard.begin();
+
+  	coolBoard.printConf();	
+
+	Serial.print("one log every ");
+	Serial.print(coolBoard.getLogInterval());
+	Serial.println(" s " );
+}
+
+void loop()
+{
+	if(coolBoard.isConnected()==0 )
+	{
+		coolBoard.onLineMode();
+	}
+	else
+	{
+		coolBoard.offLineMode();
+	}
+
+
+ 
+}
