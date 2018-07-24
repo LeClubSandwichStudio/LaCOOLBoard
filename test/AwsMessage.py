018508c980bffcac8f05c8e6d9d919c03c7d461e
gui4d <g.ducombs@free.fr>
2018-07-24 17:04:22 +0200

    add printjson  function

--- test/AwsMessage.py
+++ test/AwsMessage.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 import boto3
-import json 
+import json
+
+def printjson( jsonmessage ):
+    jsonmessage.split()
+    nbTab = 0 
+    for i in jsonmessage: 
+        if( "{" in i ):
+            nbTab = nbTab +1
+            print( i , end="" )
+        elif ("}" in i ): 
+            nbTab = nbTab -1
+            print(i + "\n" + "  "*nbTab ,end="" ) 
+        else: 
+            print(i ,end="" )
 
 client = boto3.client('iot-data', region_name='eu-west-1')
 response = client.get_thing_shadow(thingName='2C3AE84FBF4F')
