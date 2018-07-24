0c24f0eddd01e3edfc30c9f16fe5e3bdf07c257a
gui4d <g.ducombs@free.fr>
2018-07-24 17:04:22 +0200

    Add function for an AWS update

--- test/AwsMessage.py
+++ test/AwsMessage.py
@@ -1,10 +1,61 @@
 #!/usr/bin/env python3
+import json
 import boto3
-import json 
+MacAddress = '2C3AE84FBF4F'
+
+
+def printjson(jsonmessage):
+    jsonmessage.split()
+    nbTab = 0
+    for i in jsonmessage:
+        if("{" in i):
+            nbTab = nbTab + 1
+            print(i, end="")
+        elif ("}" in i:
+            nbTab=nbTab - 1
+            print(i + "\n" + "  " * nbTab, end="")
+        else:
+            print(i, end="")
+
+
+def sendMessageLogInterval():
+    client.update_thing_shadow(
+        thingName=MacAddress,
+        payload=b"""'{"state":
+                        {"desired":
+                            {"CoolBoard":
+                                {logInterval":306
+                                }
+                            }
+                        }
+                    }'"""
+        )
+
+
+#def sendMessageFirmwareVersion():
+#    client.update_thing_shadow(
+#        thingName=MacAddress,
+#        payload=b"""'{
+#                    "state": {
+#                        "desired": {
+#                            "CoolBoard": {
+#                                "firmwareUpdate": {
+#                                    "firmwareUrlFingerprint" : "BC5445C5CE60988076FFFE5C83555949810370A1",
+#                                    "firmwareUrl" : "https://s3-eu-west-1.amazonaws.com/cool-firmware-releases/feature/test-bench/debug-v0.2.7-14-gdc7c463.bin",
+#                                    "firmwareVersion" : "v0.2.7-14-gdc7c463"
+#                                }
+#                            }
+#                        }
+#                    }
+#                }'"""
+#        )
+
 
 client = boto3.client('iot-data', region_name='eu-west-1')
-response = client.get_thing_shadow(thingName='2C3AE84FBF4F')
+response = client.get_thing_shadow(thingName=MacAddress)
 streamingBody = response["payload"]
 rawDataBytes = streamingBody.read()
 rawDataString = rawDataBytes.decode('utf-8')
 jsonState = json.loads(rawDataString)
+sendMessageLogInterval()
+# sendMessageFirmwareVersion()
